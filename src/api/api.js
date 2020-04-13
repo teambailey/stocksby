19e94d04aac598bbf931a84195b648e6a1d1e072
Andrew Bailey <public7727@gmail.com>
2020-04-12 23:37:01 -0400

    Update api test file to use the new .env for the api key

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_KEY_QUERY = '?apiKey=toL8QYguX3mVMAttkRL4f_qVF2zNhd11FL_wcV';
-
 const BASE_URL_V1 = 'https://api.polygon.io/v1';
 
 const getApi = () => {
-  axios.get(`${BASE_URL_V1}/last/stocks/AAPL${API_KEY_QUERY}`).then((res) => {
-    console.log(res);
-  });
+  axios
+    .get(
+      `${BASE_URL_V1}/last/stocks/AAPL?apiKey=${process.env.REACT_APP_API_KEY}`
+    )
+    .then((res) => {
+      console.log(res);
+    });
   console.log('sup');
 };
 
