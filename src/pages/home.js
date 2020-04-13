729a3e2fccd33fdd639f68f7230d0bf5369a41f6
Andrew Bailey <public7727@gmail.com>
2020-04-12 23:39:11 -0400

    More testing with the dispatch hook

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { incrementCounter, decrementCounter } from '../redux/actions';
 
 const Home = () => {
+  const counter = useSelector((state) => state.counterReducer.counter);
+  const symbol = useSelector((state) => state.symbolReducer.symbol);
+
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Home Sup</h1>
+
+      <h1>Counter: {counter}</h1>
+      <p>Here: {symbol}</p>
+      <button onClick={() => dispatch(incrementCounter(counter))}>
+        INCREMENT
+      </button>
+      <button onClick={() => dispatch(decrementCounter(counter))}>
+        DECREMENT
+      </button>
     </div>
   );
 };
