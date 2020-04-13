3be7faa1c1fb2f2b37a009fabcaecd4b88126bfe
Andrew Bailey <public7727@gmail.com>
2020-04-12 23:38:19 -0400

    Added input and store connections for stock symbol

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveStock } from '../redux/actions';
 
 const About = () => {
+  const stockSymbol = useSelector((state) => state.symbolReducer.symbol);
+  const dispatch = useDispatch();
+
+  const [symbol, setSymbol] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (symbol === '') return;
+    dispatch(setActiveStock(symbol));
+    setSymbol('');
+  };
+
+  const onChange = (e) => setSymbol(e.target.value);
+
   return (
     <div>
-      <h1>About sup</h1>
+      <h1>About {stockSymbol}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="stock-symbol"
+          placeholder="Enter Stock Symbol"
+          value={symbol}
+          onChange={onChange}
+        />
+        <input type="submit" value="Enter" />
+      </form>
     </div>
   );
 };
