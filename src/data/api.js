07c14d045682d13fbb5763839e9a999bdfa174c9
Andrew Bailey <public7727@gmail.com>
2020-04-14 13:47:01 -0400

    Added more api calls

--- src/data/api.js
+++ src/data/api.js
@@ -6,14 +6,41 @@ const BASE_URL_V1 = `https://api.polygon.io/v1/`;
 export const getTickerDetails = (stock, callback) => {
   axios
     .get(`${BASE_URL_V1}meta/symbols/${stock}/company?${API_KEY_QUERY}`)
-    .then(callback);
+    .then(callback)
+    .catch((err) => console.log(err));
 };
+
 // Ticker News
 export const getTickerNews = (stock, callback) => {
   axios
     .get(
       `${BASE_URL_V1}meta/symbols/${stock}/news?perpage=10&page=1&${API_KEY_QUERY}`
     )
-    // .then((res) => console.log(res));
-    .then(callback);
+    .then(callback)
+    .catch((err) => console.log(err));
+};
+
+// Last trade for symbol
+export const getLastTrade = (stock, callback) => {
+  axios
+    .get(`${BASE_URL_V1}last/stocks/${stock}?${API_KEY_QUERY}`)
+    .then(callback)
+    .catch((err) => console.log(err));
+};
+
+// Last quote for symbol
+export const getLastQuote = (stock, callback) => {
+  axios
+    .get(`${BASE_URL_V1}last_quote/stocks/${stock}?${API_KEY_QUERY}`)
+    .then(callback)
+    .catch((err) => console.log(err));
+};
+
+// Daily Open / Close
+// *** Endpoint failed to return when testing at https://polygon.io/docs#get_v1_open-close__symbol___date__anchor
+export const getOpenClose = (stock, callback) => {
+  axios
+    .get(`${BASE_URL_V1}open-close/${stock}/2020-4-15?${API_KEY_QUERY}`)
+    .then(callback)
+    .catch((err) => console.log(err));
 };
