7bcb8374d415f983a5bd07c652e75d1b341d9057
Andrew Bailey <public7727@gmail.com>
2020-04-14 16:00:08 -0400

    Removed second page link

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,14 +1,8 @@
 // Pages
 import Dashboard from './pages/dashboard';
-import About from './pages/about';
 
 // Routing config
 const Routes = [
-  {
-    path: '/about',
-    component: About,
-    linkText: 'About',
-  },
   {
     // index must be last in the array
     path: '/',
