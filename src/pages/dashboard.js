c36aca414f109e26665cccf4ce0617449af2d1e6
Andrew Bailey <public7727@gmail.com>
2020-04-14 13:47:40 -0400

    Added more components

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getTickerDetails, getTickerNews } from '../data/api';
-import { setActiveStockDetails, setActiveStockNews } from '../redux/actions';
+import { setActiveStockDetails } from '../redux/actions';
+import {
+  getTickerDetails,
+  getTickerNews,
+  getLastTrade,
+  getLastQuote,
+  // getOpenClose,
+} from '../data/api';
 import Hero from '../components/Hero';
-
-// Page variables
-const config = {
-  hero: {
-    title: 'Dashboard',
-    subtitle: 'Subtile',
-  },
-};
+import CurrentInfoBox from '../components/CurrentinfoBox';
+import NewsBox from '../components/NewsBox';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const tickerSymbol = useSelector((state) => state.searchReducer.symbol);
+
   const [details, setDetails] = useState({});
   const [news, setNews] = useState([]);
+  const [lastTrade, setLastTrade] = useState();
+  const [lastQuote, setLastQuote] = useState();
 
-  const getDetails = (res) => {
+  const buildDetails = (res) => {
     setDetails({
       logo: res.data.logo,
       url: res.data.url,
@@ -30,53 +33,57 @@ const Dashboard = () => {
     });
   };
 
-  const getNews = (res) => setNews(res.data);
+  const buildNews = (res) => setNews(res.data);
+  const buildLastTrade = (res) => setLastTrade(res.data.last.price);
+  const buildLastQuote = (res) => setLastQuote(res.data.last.askprice);
+  // const buildOpenClose = (res) => setOpenClose(res.data);
 
   useEffect(() => {
-    getTickerDetails(tickerSymbol, getDetails);
-    getTickerNews(tickerSymbol, getNews);
+    getTickerDetails(tickerSymbol, buildDetails);
+    getTickerNews(tickerSymbol, buildNews);
+    getLastTrade(tickerSymbol, buildLastTrade);
+    getLastQuote(tickerSymbol, buildLastQuote);
+    // getOpenClose(tickerSymbol, buildOpenClose); // THIS IS NOT RETURNING FROM THE ENDPOINT
   }, [tickerSymbol]);
 
   useEffect(() => {
     dispatch(setActiveStockDetails(details));
   }, [dispatch, details]);
 
-  useEffect(() => {
-    dispatch(setActiveStockNews(news));
-    if (news[0]) console.log(news[0].url);
-  }, [dispatch, news]);
-
   return (
     <div className="container">
-      <p>Here: {details.logo}</p>
-      <p>Here: {details.url}</p>
-      <p>Here: {details.desc}</p>
-      <p>Here: {details.name}</p>
-      <p>Here: {details.symbol}</p>
-      <p>Here: {details.industry}</p>
-      <p>Here: {details.employees}</p>
-      <hr />
-      <p>Here: {news.length > 0 ? news[0].url : ''}</p>
-      <Hero title={tickerSymbol} subtitle={config.hero.subtitle} />
+      {/* <p>{news.length > 0 ? news[0].url : ''}</p> */}
+      <Hero
+        image={details.logo}
+        title={details.symbol}
+        subtitle={details.name}
+        lastTrade={lastTrade}
+      />
 
-      {/* <div className="level">
-        <div className="level-item has-text-centered">
-          <div>
-            <p className="heading">TICKER SYMBOL</p>
-            <p className="title">{tickerSymbol || 'N/A'}</p>
+      <div className="section has-background-grey-lighter">
+        <div className="columns">
+          <div className="column">
+            <CurrentInfoBox heading="Last Trade" title={lastTrade} />
+          </div>
+          <div className="column">
+            <CurrentInfoBox heading="Open" title="280" />
+          </div>
+          <div className="column">
+            <CurrentInfoBox heading="Close" title="273.25" />
+          </div>
+          <div className="column">
+            <CurrentInfoBox heading="Last Quote" title={lastQuote} />
           </div>
         </div>
-      </div> */}
+      </div>
 
-      {/* <div className="section">
-        <div className="container">
-          <div className="columns is-desktop">
-            <div className="column">1</div>
-            <div className="column">2</div>
-            <div className="column">3</div>
-          </div>
-        </div>
-      </div> */}
+      <hr className="has-background-grey-lighter	" />
+
+      <div className="section has-background-grey-lighter">
+        {news.map((item, i) => (
+          <NewsBox key={i} news={item} />
+        ))}
+      </div>
     </div>
   );
 };
