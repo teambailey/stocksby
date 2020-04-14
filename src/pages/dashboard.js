fe5c041b6e36a46d6de8c84a10f469aa500476d5
Andrew Bailey <public7727@gmail.com>
2020-04-14 06:08:25 -0400

    Pull data to display from api and setup the storeage

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import Hero from '../components/Hero';
-
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getTickerDetails, getTickerNews } from '../data/api';
+import { setActiveStockDetails, setActiveStockNews } from '../redux/actions';
+import Hero from '../components/Hero';
 
 // Page variables
 const config = {
@@ -13,15 +13,50 @@ const config = {
 };
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
   const tickerSymbol = useSelector((state) => state.searchReducer.symbol);
+  const [details, setDetails] = useState({});
+  const [news, setNews] = useState([]);
+
+  const getDetails = (res) => {
+    setDetails({
+      logo: res.data.logo,
+      url: res.data.url,
+      desc: res.data.description,
+      name: res.data.name,
+      symbol: res.data.symbol,
+      industry: res.data.industry,
+      employees: res.data.employees,
+    });
+  };
+
+  const getNews = (res) => setNews(res.data);
 
   useEffect(() => {
-    getTickerDetails(tickerSymbol);
-    getTickerNews(tickerSymbol);
+    getTickerDetails(tickerSymbol, getDetails);
+    getTickerNews(tickerSymbol, getNews);
   }, [tickerSymbol]);
+
+  useEffect(() => {
+    dispatch(setActiveStockDetails(details));
+  }, [dispatch, details]);
+
+  useEffect(() => {
+    dispatch(setActiveStockNews(news));
+    if (news[0]) console.log(news[0].url);
+  }, [dispatch, news]);
 
   return (
     <div className="container">
+      <p>Here: {details.logo}</p>
+      <p>Here: {details.url}</p>
+      <p>Here: {details.desc}</p>
+      <p>Here: {details.name}</p>
+      <p>Here: {details.symbol}</p>
+      <p>Here: {details.industry}</p>
+      <p>Here: {details.employees}</p>
+      <hr />
+      <p>Here: {news.length > 0 ? news[0].url : ''}</p>
       <Hero title={tickerSymbol} subtitle={config.hero.subtitle} />
 
       {/* <div className="level">
